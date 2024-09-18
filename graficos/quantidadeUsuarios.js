bc2b73208e7028bcfcdea5dcc14cdda568cca857
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-09-18 15:41:59 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,12 +4,12 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
+    const nomeDostimes = Object.keys(dados)
     const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
-            x: nomeDasRedes, 
+            x: nomeDostimes, 
             y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
